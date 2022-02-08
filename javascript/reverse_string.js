21eba948afb1db08740a14d00c566904427e91cd
dominick <dominickugas@gmail.com>
2022-02-07 21:52:09 -0500

    done

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,17 @@
+// function reverseString(str) {
+//   // type your code here
+//   let output = ""
+//   for (let i = str.length -1; i >= 0; i--) {
+//     output += str[i]
+//   }
+//   return output
+// }
 function reverseString(str) {
-  // type your code here
+  let output = []
+  for (let i = 0; i <= str.length; i++) {
+    output.unshift(str[i])
+  }
+  return output.join("")
 }
 
 if (require.main === module) {
@@ -17,3 +29,7 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// 1.seperate the string into characters 
+// 2.reverse the order of the characters
+// 3. join the character together
